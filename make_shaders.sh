1978eeb71a6e42618c59a6ab84fc716165ebb958
Chris <chrisnonymous2000@gmail.com>
2024-02-09 15:31:31 -0700

    Cmake support, fix makefile

---
+++ make_shaders.sh
@@ -0,0 +1,4 @@
+# run this with your cmake output directory as the argument before calling cmake!
+
+picasso -o "$1"/clear.shbin source/shaders/clear.v.pica
+picasso -o "$1"/vshader.shbin source/shaders/vshader.v.pica
