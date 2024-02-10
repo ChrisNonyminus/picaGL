dd4c1892439269a95552b4555d3996f753841796
ChrisNonyminus <50080567+ChrisNonyminus@users.noreply.github.com>
2024-02-09 18:09:34 -0700

    Fix fix for gluPerspective linkage

--- include/GL/glu.h
+++ include/GL/glu.h
@@ -1,9 +1,14 @@
 #ifndef GLU_H_
 #define GLU_H_
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 void gluPerspective(float fovy, float aspect, float near, float far);
+
+#ifdef __cplusplus
 }
+#endif
 
 
 #endif
